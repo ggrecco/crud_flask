f3cd83d8b7a3bdef30fcbc637d49d2050f0c4190
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-07-20 18:42:24 -0300

    passando um dicionario para o template

--- crf/app/routes.py
+++ crf/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    inicio = {'frase': 'crud-flask'}
+    return render_template('index.html', title='Crud-Flask', inicio=inicio)
