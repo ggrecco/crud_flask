1f3ada2b6aa4c194df1e8cbbf41609af81113653
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-07-23 11:38:49 -0300

    adicionado campo permissions e atualizado o banco de dados

--- crf/app/models.py
+++ crf/app/models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    permissions = db.Column(db.String(3))
 
     def __repr__(self):
         return '<Usuario {}>'.format(self.username)
